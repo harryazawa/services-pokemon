69996b7d4fbfd7c0c4dc6fede75dff01d62fb8ed
harryazawa <victorlbamorim@gmail.com>
2022-12-13 15:14:56 -0300

    feat: added dynamic pokemon search

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PokemonService} from "../../services/pokemon.service";
+import {PokemonData} from "../../models/pokemonData";
 
 @Component({
   selector: 'app-card',
@@ -7,13 +8,32 @@ import {PokemonService} from "../../services/pokemon.service";
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name:string = "BULBASAUR"
-  attributesTypes:string[] = ['FIRE', 'ROCK']
+  pokemon:PokemonData
   constructor(
     private service:PokemonService
-  ) { }
-
-  ngOnInit(): void {
+  ) {
+    this.pokemon = { id:0, name:'', sprites:{ front_default: '' }, types:[]
+    }
   }
 
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        },
+        error: (err) => console.log(err)
+      }
+    )
+  }
 }
